4f361ca06bbcc1fad3dca353196d126780011e44
mwormit <mwormit@b61ddc4e-6d22-4b3b-b2dc-79edcf157fa9>
2014-01-14 09:36:12 +0000

    Interfaces for exporting densities and orbitals

---
+++ libwfa/export_densities_i.h
@@ -0,0 +1,36 @@
+#ifndef LIBWFA_EXPORT_DENSITIES_I_H
+#define LIBWFA_EXPORT_DENSITIES_I_H
+
+#include "ab_matrix.h"
+#include "density_list.h"
+
+namespace libwfa {
+
+/** \brief Interface for exporting density matrices.
+
+    Interface to implement the export of density matrices.
+
+    \ingroup libwfa
+ **/
+class export_densities_i {
+public:
+    virtual ~export_densities_i() { }
+
+    /** \brief Export the density matrix
+        \param type Type of density matrix
+        \param idx Index of density matrix
+        \param dm Density matrix
+     **/
+    virtual void perform(density_type type, size_t idx,
+            const ab_matrix &dm) = 0;
+
+    /** \brief Export the list of density matrices
+        \param lst Density matrix list
+     **/
+    virtual void perform(const density_list &lst) = 0;
+};
+
+
+} // namespace libwfa
+
+#endif // LIBWFA_EXPORT_DENSITIES_I_H
---
+++ libwfa/export_orbitals_i.h
@@ -0,0 +1,32 @@
+#ifndef LIBWFA_EXPORT_ORBITALS_I_H
+#define LIBWFA_EXPORT_ORBITALS_I_H
+
+#include "ab_matrix.h"
+#include "ab_vector.h"
+#include "ab_selector.h"
+
+namespace libwfa {
+
+/** \brief Interface for exporting orbital data.
+
+    Interface to implement for export of orbital data to a file.
+
+    \ingroup libwfa
+ **/
+class export_orbitals_i {
+public:
+    virtual ~export_orbitals_i() { }
+
+    /** \brief Write the given orbital coefficients to the file.
+        \param coeff Orbital coefficients
+        \param ene Orbital energies
+        \param s Selector of specific orbital coefficients
+     **/
+    virtual void perform(const ab_matrix &coeff,
+            const ab_vector &ene, const ab_selector &s) = 0;
+};
+
+
+} // namespace libwfa
+
+#endif // LIBWFA_EXPORT_ORBITALS_I_H
