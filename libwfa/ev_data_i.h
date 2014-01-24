10ccc1706a052e619c77c1de008e9e8269c5548b
mwormit <mwormit@b61ddc4e-6d22-4b3b-b2dc-79edcf157fa9>
2014-01-24 18:05:54 +0000

    Added missing header ev_data_i.h

---
+++ libwfa/ev_data_i.h
@@ -0,0 +1,30 @@
+#ifndef LIBWFA_EV_DATA_I_H
+#define LIBWFA_EV_DATA_I_H
+
+#include <iostream>
+#include <list>
+#include "ab_vector.h"
+#include "dm_type.h"
+
+namespace libwfa {
+
+/** \brief Interface class to analyse eigenvalues of density matrices
+
+    \ingroup libwfa
+ **/
+class ev_data_i {
+public:
+    virtual ~ev_data_i() { }
+
+    /** \brief Perform the operation
+        \param type Type of density matrix
+        \param ni Occupation number vector (= vector of eigenvalues)
+        \return Number of important occupation numbers
+     **/
+    virtual size_t perform(dm_type type, const ab_vector &ni) = 0;
+};
+
+
+} // namespace adcman
+
+#endif // LIBWFA_EV_DATA_I_H
