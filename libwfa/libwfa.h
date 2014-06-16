0ed01f4588640e416ef920743e80f17ea0e4167f
fplasser <fplasser@b61ddc4e-6d22-4b3b-b2dc-79edcf157fa9>
2014-06-16 15:01:36 +0000

    mainpage doc

--- libwfa/libwfa.h
+++ libwfa/libwfa.h
@@ -3,10 +3,44 @@
 
 /** \mainpage Wave-function analysis tool library
 
-    The library consists of a number of tools for wave function or rather
-    density matrix analysis.
+    libwfa provides a C++ library for the analysis of wavefunctions. This
+    analysis is based on density matrices, which provide a unified formalism
+    independent of the wavefunction model.
 
-    ... to be continued ...
+    The methods used have been described in detail in Ref. [1] and examples are
+    provided in Ref. [2].
+    Briefly, the implemented methods consist of the following parts:
+
+    1. Analysis of state density matrices for
+       - population analysis,
+       - density plotting,
+       - natural orbitals, and
+       - analysis of unpaired electrons.
+
+    2. Analysis of transition density matrices for
+       - plotting of the \a hole and \a electron densities,
+       - plotting the transition density,
+       - natural transition orbitals (with a possibility for state-averaging), and
+       - \a electron-hole correlation analysis using the charge transfer numbers (see also Ref. [3]).
+
+    3. Analysis of difference density matrices for
+       - plotting of the attachment/detachment densities,
+       - attachment/detachment population analysis, and
+       - natural difference orbitals.
+
+    The required input from the quantum chemical program consists of the
+    density matrices of interest, the AO-overlap matrix, the MO coefficients
+    (used for orthogonalization), and information for population analysis.
+
+    Current developments: multipole analysis.
+
+    \b Literature
+
+    [1] Plasser, F.; Wormit, M.; Dreuw, A. <em>submitted for publication</em>
+
+    [2] Plasser, F.; Baeppler, S.A.; Wormit, M.; Dreuw, A. <em>submitted for publication</em>
+
+    [3] Plasser, F.; Lischka, H. \a JCTC, \b 2012, \a 8, 2777.
  **/
 
 /** \defgroup libwfa Library of wave-function analysis tools
