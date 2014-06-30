f1f7cac5487dc605c4c60c82fc4f24d8c413ec17
mwormit <mwormit@b61ddc4e-6d22-4b3b-b2dc-79edcf157fa9>
2014-06-30 00:28:35 +0000

    Fixes in pop_analysis_ad + tests

--- libwfa/analyses/pop_analysis_ad.C
+++ libwfa/analyses/pop_analysis_ad.C
@@ -10,21 +10,21 @@ void pop_analysis_ad::perform(pop_data &pop) const {
     if (m_at.is_alpha_eq_beta()) {
 
         Col<double> &h = pop.add("h+");
-        m_analysis.perform(m_de.alpha(), h);
+        m_analysis.perform(m_de.alpha() * -2., h);
         Col<double> &e = pop.add("e-");
-        m_analysis.perform(m_at.alpha(), e);
+        m_analysis.perform(m_at.alpha() *  2., e);
         Col<double> &dq = pop.add("Del q");
         dq = h + e;
     }
     else {
         Col<double> &ha = pop.add("h+ (alpha)");
-        m_analysis.perform(m_de.alpha(), ha);
+        m_analysis.perform(m_de.alpha() * -1., ha);
         Col<double> &hb = pop.add("h+ (beta)");
-        m_analysis.perform(m_de.beta(), hb);
+        m_analysis.perform(m_de.beta() * -1., hb);
         Col<double> &ea = pop.add("e- (alpha)");
         m_analysis.perform(m_at.alpha(), ea);
         Col<double> &eb = pop.add("e- (beta)");
-        m_analysis.perform(m_at.alpha(), eb);
+        m_analysis.perform(m_at.beta(), eb);
     }
 }
 
