f214d708978b91213507b08d018c82e6923b5bab
Felix Plasser <felix.plasser@univie.ac.at>
2016-03-09 11:17:33 +0100

    write some attributes (could be improved), #12

--- libwfa/molcas/molcas_export_h5orbs.C
+++ libwfa/molcas/molcas_export_h5orbs.C
@@ -8,10 +8,10 @@ void molcas_export_h5orbs::perform(const std::string &name,
     const arma::mat &c, const arma::vec &e, size_t nocc) {
 
     // Export coefficients
-    h5_vec("DESYM_" + name + "_VECTORS", vectorise(c));
+    h5_coeff(name, c);
 
     // Export eigenvalues - these are interpreted as occupations
-    h5_vec("DESYM_" + name + "_OCCUPATIONS", e);
+    h5_occs(name, e);
 }
 
 void molcas_export_h5orbs::perform(const std::string &name,
@@ -19,19 +19,36 @@ void molcas_export_h5orbs::perform(const std::string &name,
     const arma::mat &c_b, const arma::vec &e_b, size_t nocc_b) {
 
     // Export alpha coefficients
-    h5_vec("DESYM_" + name + "_ALPHA_VECTORS", vectorise(c_a));
+    h5_coeff(name + "_ALPHA", c_a);
 
     // Export eigenvalues - these are interpreted as occupations
-    h5_vec("DESYM_" + name + "_ALPHA_OCCUPATIONS", e_a);
+    h5_occs(name + "_ALPHA", e_a);
 
     // Export beta coefficients
-    h5_vec("DESYM_" + name + "_BETA_VECTORS", vectorise(c_b));
+    h5_coeff(name + "_BETA", c_b);
 
     // Export eigenvalues - these are interpreted as occupations
-    h5_vec("DESYM_" + name + "_BETA_OCCUPATIONS", e_b);
+    h5_occs(name + "_BETA", e_b);
 }
 
-void molcas_export_h5orbs::h5_vec(H5std_string oname, const arma::vec data) {
+void molcas_export_h5orbs::setup(H5File &file) {
+        try {
+            m_group = file.createGroup("/WFA");
+
+            H5std_string descr = "Wavefunction analysis data produced by molcas_wfa.x";
+            DataSpace Space(H5S_SCALAR);
+            StrType strtype(PredType::C_S1, descr.size());
+
+            Attribute Att = m_group.createAttribute("DESCRIPTION", strtype, Space);
+            Att.write(strtype, descr);
+        }
+        catch( H5::FileIException error ) {
+            std::cout << std::endl << "WARNING: Overwriting data in existing group WFA " << std::endl;
+            m_group = file.openGroup("/WFA");
+        }
+}
+
+void molcas_export_h5orbs::h5_vec(H5std_string oname, const H5std_string descr, const arma::vec data) {
     std::transform(oname.begin(), oname.end(), oname.begin(), ::toupper);
 
     const hsize_t dim = data.size();
@@ -41,8 +58,19 @@ void molcas_export_h5orbs::h5_vec(H5std_string oname, const arma::vec data) {
 
     try {
         Exception::dontPrint();
-        DataSpace Space(rank, &dim);
-        Set = m_group.createDataSet(oname, PredType::NATIVE_DOUBLE, Space);
+
+        {
+            DataSpace Space(rank, &dim);
+            Set = m_group.createDataSet(oname, PredType::NATIVE_DOUBLE, Space);
+        }
+        { // Create a description as attribute
+            DataSpace Space(H5S_SCALAR);
+            StrType strtype(PredType::C_S1, descr.size());
+
+            Attribute Att = Set.createAttribute("DESCRIPTION", strtype, Space);
+            Att.write(strtype, descr);
+        }
+
     }
     catch( GroupIException error ) { // open and overwrite the dataset if it already exists
         //std::cout << "WARNING: Overwriting existing dataset " << oname << std::endl;
@@ -50,5 +78,16 @@ void molcas_export_h5orbs::h5_vec(H5std_string oname, const arma::vec data) {
     }
     Set.write(dataptr, PredType::NATIVE_DOUBLE);
 }
+
+void molcas_export_h5orbs::h5_coeff(H5std_string oname, const arma::mat c) {
+    H5std_string descr = "Desymmetrized orbital coefficients";
+    h5_vec("DESYM_" + oname + "_VECTORS", descr, vectorise(c));
+}
+
+void molcas_export_h5orbs::h5_occs(H5std_string oname, const arma::vec e) {
+    H5std_string descr = "Orbital eigenvalues";
+    h5_vec("DESYM_" + oname + "_OCCUPATIONS", descr, e);
+}
+
 
 } // namespace libwfa
--- libwfa/molcas/molcas_export_h5orbs.h
+++ libwfa/molcas/molcas_export_h5orbs.h
@@ -7,7 +7,7 @@
 namespace libwfa {
 
 /** \brief Export to HDF5 file object for Molcas.
- 
+
     This is based on the export_molden functionality of libwfa, since the
     required routines are similar.
 
@@ -19,20 +19,14 @@ private:
     H5::Group  m_group; //!< Group on HDF5 file
     const arma::uvec m_nbas; //!< Number of basis functions per irrep
     const arma::mat  m_desym; //!< Desymmetrization matrix
-    
+
 public:
     /** \brief Constructor
         \param prefix Prefix of h5file (directory)
      **/
     molcas_export_h5orbs(H5::H5File &file, const arma::uvec nbas, const arma::mat desym) :
         m_nbas(nbas), m_desym(desym) {
-        try {
-            m_group = file.createGroup("WFA");
-        }
-        catch( H5::FileIException error ) {
-            std::cout << std::endl << "WARNING: Overwriting data in existing group WFA " << std::endl;
-            m_group = file.openGroup("WFA");
-        }
+        setup(file);
     }
 
     /** \brief Virtual destructor
@@ -50,8 +44,10 @@ public:
     //@}
 
 private:
-    arma::vec desym(const arma::mat c);
-    void h5_vec(const H5std_string oname, const arma::vec data);
+    void setup(H5::H5File &file);
+    void h5_vec(const H5std_string oname, const H5std_string descr, const arma::vec data);
+    void h5_coeff(const H5std_string oname, const arma::mat c);
+    void h5_occs(const H5std_string oname, const arma::vec e);
 };
 
 
