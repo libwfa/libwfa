d6a51afaddd5f745b72a32b5a614cdfc009a9a5b
Felix Plasser <felix.plasser@univie.ac.at>
2016-07-28 14:50:46 +0200

    allow compilation of standalone executable wfa_molcas.x

--- libwfa/molcas/driver.C
+++ libwfa/molcas/driver.C
@@ -1,10 +1,8 @@
-// #include "H5Cpp.h"
 #include <libwfa/libwfa.h>
 #include <libwfa/wf_analysis.h>
 #include <libwfa/molcas/molcas_wf_analysis.h>
 #include <libwfa/molcas/molcas_wf_analysis_data.h>
 
-// using namespace H5;
 using namespace libwfa;
 
 int wfa_driver()
--- libwfa/molcas/main.C
+++ libwfa/molcas/main.C
@@ -1,55 +1,19 @@
-#include "H5Cpp.h"
-#include <libwfa/molcas/molcas_wf_analysis.h>
 #include <libwfa/libwfa.h>
+#include <libwfa/wf_analysis.h>
+#include <libwfa/molcas/molcas_wf_analysis.h>
 #include <libwfa/molcas/molcas_wf_analysis_data.h>
 
-using namespace H5;
 using namespace libwfa;
 
 int main(int argc, char** argv)
 {
-    H5std_string file_name("molcas.rasscf.h5");
-    size_t refstate = 0;
-    if (argc>=2){
-        file_name = argv[1];
-        if (argc>=3) {
-            refstate = atoi(argv[2]) - 1;
-        }
-    }
 #ifdef LIBWFA_DEBUG
     std::cout << "*** Debug mode activated ***" << std::endl << std::endl;
 #endif
-    std::cout << "Starting analysis of Molcas HDF5 file " << file_name << std::endl;
 
-    H5File file( file_name, H5F_ACC_RDWR );
-
-    molcas_wf_analysis_data *wfdata = libwfa::molcas_setup_wf_analysis_data(file);
+    molcas_wf_analysis_data *wfdata = libwfa::molcas_setup_wf_analysis_data();
     molcas_wf_analysis wf(wfdata);
+    wf.run_analysis();
 
-    // Check what kind of job was performed
-    H5std_string molcas_module;
-    {
-        Attribute Att = file.openGroup("/").openAttribute("MOLCAS_MODULE");
-        StrType strtype(PredType::C_S1, 16);
-        Att.read(strtype, molcas_module);
-    }
-
-    if (molcas_module=="SCF") {        
-        wf.scf_analysis();
-    }
-    else if (molcas_module=="RASSCF") {
-        wf.rasscf_analysis(refstate);
-    }
-    else if (molcas_module=="RASSI") {
-        wf.rassi_analysis(refstate);
-    }
-    else {
-        std::ostringstream os;
-        os << std::endl << "Unknown MOLCAS module: " << molcas_module;
-        const std::string errmsg = os.str();
-        throw libwfa_exception("main", "main", __FILE__, __LINE__, errmsg.c_str());
-    }
-    
-    std::cout << "Module wfa finished." << std::endl;
     return 0;
 } // main
