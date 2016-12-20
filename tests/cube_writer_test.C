78c3e307d3118593a64521f739085f296dd3dce1
Michael F. Herbst <info@michael-herbst.com>
2016-12-20 23:17:11 +0100

    Make armadillo 7.5 and gcc 6.2

--- tests/cube_writer_test.C
+++ tests/cube_writer_test.C
@@ -23,11 +23,11 @@ void cube_writer_test::test_1() {
 
     try {
 
-    unsigned int atnum[2] = { 8, 8 };
+    uword atnum[2] = { 8, 8 };
     double coord[6] = { 0.0, 0.0, -1.2, 0.0, 0.0, 1.2 };
     grid3d g(5, 0.5);
     cube_writer wr("cube_writer_test_1.cube", testname, "O2 test", g,
-            Col<unsigned int>(atnum, 2), mat(coord, 3, 2));
+            Col<uword>(atnum, 2), mat(coord, 3, 2));
 
     if (wr.npoints() != 125) fail_test(testname, __FILE__, __LINE__, "npts");
 
