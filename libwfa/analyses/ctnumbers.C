c24e28f59dbeea0b534ba85409d873a7a2a040fd
Patrick Kimber <P.Kimber@lboro.ac.uk>
2019-10-15 10:29:28 +0100

    Add electron/hole permutation, #8

--- libwfa/analyses/ctnumbers.C
+++ libwfa/analyses/ctnumbers.C
@@ -12,10 +12,14 @@ ctnumbers::ctnumbers(const ctnum_analysis_i &a, const ab_matrix &tdm) :
 
     a.perform(tdm.alpha(), m_om.alpha());
     m_tot[0] = m_tot[1] = accu(m_om.alpha());
+    m_Phe[0] = m_Phe[1] = accu(tdm.alpha() % tdm.alpha().t());
+//    std::cout << "tmp: " << trace(tdm.alpha() * tdm.alpha())  << std::endl;
+//    std::cout << "tmp2: " <<  accu(tdm.alpha() % tdm.alpha().t()) << std::endl;
 
     if (! m_om.is_alpha_eq_beta()) {
         a.perform(tdm.beta(), m_om.beta());
         m_tot[1] = accu(m_om.beta());
+        m_Phe[1] = accu(tdm.beta() % tdm.beta().t());
     }
 }
 
@@ -33,10 +37,18 @@ void ctnumbers::analyse(std::ostream &out) const {
         out << std::setw(w) << m_tot[0] << ", beta: ";
         out << std::setw(w) << m_tot[1] << ")" << std::endl;
     }
+
+    out << "  <Phe> = ";
+    out << std::setprecision(prec) << std::fixed;
+    if (m_om.is_alpha_eq_beta()) {
+        out << std::setw(w) << (m_Phe[0] + m_Phe[1]) / (m_tot[0] + m_tot[1]) << std::endl;
+    }
+    else {
+        out << std::setw(w) << (m_Phe[0] + m_Phe[1]) / (m_tot[0] + m_tot[1]) << " (alpha: ";
+        out << std::setw(w) << m_Phe[0] / m_tot[0] << ", beta: ";
+        out << std::setw(w) << m_Phe[1] / m_tot[1]  << ")" << std::endl;
+    }
 }
 
 
 } // namespace libwfa
-
-
-
