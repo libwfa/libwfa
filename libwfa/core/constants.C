7229a9db3eaf67c612526f431128e1023a1e4daa
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2021-11-26 09:02:39 +0000

    merge

--- libwfa/core/constants.C
+++ libwfa/core/constants.C
@@ -2,11 +2,11 @@
 
 namespace libwfa {
 
-const double constants::au2ang = 0.529177208;
-const double constants::au2nm =  0.0529177208;
-const double constants::au2eV = 27.211396132;
-const double constants::au2rcm = 219474.63;
-const double constants::au2D  = 2.54174619;
+const double constants::au2ang = 0.529177210903;
+const double constants::au2nm =  0.0529177210903;
+const double constants::au2eV =  27.211386245988;
+const double constants::au2rcm = 219474.6313632;
+const double constants::au2D  = 2.5417463637301756;
 const double constants::alpha = 0.0072973525693;
 
 }
